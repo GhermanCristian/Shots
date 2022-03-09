ea2d30475c34dbc2b9d81e92687d41d6d55dcd3a
GhermanCristian <gherman_cristi10@yahoo.com>
2022-03-09 13:08:44 +0200

    Refactoring in PlayerScript

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -19,25 +19,27 @@ public class PlayerScript : MonoBehaviour {
         return player.transform.position;
     }
 
-    private bool isBulletActive() {
-        return currentBulletClone != null && currentBulletClone.isAlive == true;
-    }
-
-    private void fireShot() {
-        this.currentBulletClone = Instantiate(bulletPrefab).GetComponent<Bullet>();
-        this.currentBulletClone.player = player;
-        gameControllerScript.fireShot(this);
-    }
-
     void Start() {
         playerRigidBody = GetComponent<Rigidbody2D>();
         gameControllerScript = GameControllerScript.getInstance();
     }
 
-    void Update() {
-        float horizontalTranslation = Input.GetAxis("Horizontal_P1");
-        float verticalTranslation = Input.GetAxis("Jump_P1");
+    private bool isBulletActive() {
+        return currentBulletClone != null && currentBulletClone.isAlive == true;
+    }
 
+    private void fireShot() {
+        if (Input.GetAxis("Fire1_P1") != 1f || this.isBulletActive()) {
+            return;
+        }
+
+        this.currentBulletClone = Instantiate(bulletPrefab).GetComponent<Bullet>();
+        this.currentBulletClone.player = player;
+        gameControllerScript.fireShot(this);
+    }
+
+    private void moveHorizontally() {
+        float horizontalTranslation = Input.GetAxis("Horizontal_P1");
         // the translation should be constant (in this case 0.33 units)
         if (horizontalTranslation > 0.15f) {
             horizontalTranslation = 0.33f;
@@ -48,23 +50,26 @@ public class PlayerScript : MonoBehaviour {
         else { // if the input is small ([-0.15, 0.15] units), the player stops
             horizontalTranslation = 0.0f;
         }
-
-        // jump
-        if (verticalTranslation > 0.5f && player.transform.position.y < HIGHEST_Y_COORD) {
-            // TODO - prevent double-jumping
-            playerRigidBody.AddForce(transform.up * 3f, ForceMode2D.Impulse);
-        }
-
         horizontalTranslation *= Time.deltaTime * PLAYER_HORIZONTAL_SPEED;
         transform.Translate(horizontalTranslation, 0, 0);
 
-        // don't allow the player to go past the screen limits
+        // prevent the player from going past the screen limits
         if (player.transform.position.x < LEFTMOST_X_COORD) {
             player.transform.position = new Vector3(LEFTMOST_X_COORD, player.transform.position.y, 0);
         }
         if (player.transform.position.x > RIGHTMOST_X_COORD) {
             player.transform.position = new Vector3(RIGHTMOST_X_COORD, player.transform.position.y, 0);
         }
+    }
+
+    private void jump() {
+        float verticalTranslation = Input.GetAxis("Jump_P1");
+        if (verticalTranslation > 0.5f && player.transform.position.y < HIGHEST_Y_COORD) {
+            // TODO - prevent double-jumping
+            playerRigidBody.AddForce(transform.up * 3f, ForceMode2D.Impulse);
+        }
+
+        // prevent the player from jumping too high or falling through the floor
         if (player.transform.position.y < LOWEST_Y_COORD) {
             player.transform.position = new Vector3(player.transform.position.x, LOWEST_Y_COORD, 0);
         }
@@ -72,9 +77,11 @@ public class PlayerScript : MonoBehaviour {
             player.transform.position = new Vector3(player.transform.position.x, HIGHEST_Y_COORD, 0);
             playerRigidBody.AddForce(transform.up * -0.7f, ForceMode2D.Impulse);
         }
+    }
 
-        if (Input.GetAxis("Fire1_P1") == 1f && this.isBulletActive() == false) {
-            this.fireShot();
-        }
+    void Update() {
+        this.moveHorizontally();
+        this.jump();
+        this.fireShot();
     }
 }
